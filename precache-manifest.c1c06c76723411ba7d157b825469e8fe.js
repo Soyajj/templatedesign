6edc522a2c1a0f2a45ba5dc08e058a456e6b28fd
Soyaj Bajracharya <coolboysoyaz@gmail.com>
2020-08-19 21:35:42 +0545

    Updates

---
+++ precache-manifest.c1c06c76723411ba7d157b825469e8fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cf575bcd0ef8ac0c94c314ff4278e4",
+    "url": "/templatedesign/index.html"
+  },
+  {
+    "revision": "2fbba17c3cef5c863a60",
+    "url": "/templatedesign/static/css/main.c68442b3.chunk.css"
+  },
+  {
+    "revision": "d196ea5ab80aeee78037",
+    "url": "/templatedesign/static/js/2.97a1212d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/templatedesign/static/js/2.97a1212d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbba17c3cef5c863a60",
+    "url": "/templatedesign/static/js/main.9d2750b4.chunk.js"
+  },
+  {
+    "revision": "2f78c6a0f0d6172a2c61",
+    "url": "/templatedesign/static/js/runtime-main.af1c41b3.js"
+  },
+  {
+    "revision": "9b1d1ef2d5f38d8c8b8eb1b70f078ef9",
+    "url": "/templatedesign/static/media/map.9b1d1ef2.jpg"
+  }
+]);
