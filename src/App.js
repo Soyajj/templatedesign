1220e48ad2aecab900b2ec0fb5f9c09f24b1243e
Soyaj Bajracharya <59200303+Soyajj@users.noreply.github.com>
2020-08-31 19:48:30 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,27 +9,26 @@ import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 class App extends Component {
   constructor(props){
     super(props);
-    this.state  = {
-      darkMode : false
+    this.state = {
+      darkMode: false
     }
   }
   
   toggler = () => {
     this.setState({
-      darkMode : !this.state.darkMode
+      darkMode: !this.state.darkMode
     })
   }  
 
   render() {
   return (
     <Router>
-      <div className = {this.state.darkMode ? 'body-dark' : 'body-light'}>
-      <i className = {this.state.darkMode ? 'fa fa-toggle-on' : 'fa fa-toggle-off' } onClick = {this.toggler} id = "toggleBtn"
+      <div className={this.state.darkMode ? 'body-dark' : 'body-light'}>
+      <i className={this.state.darkMode ? 'fa fa-toggle-on' : 'fa fa-toggle-off' } onClick={this.toggler} id="toggleBtn"
       >{this.state.darkMode ? 'Dark Mode' : 'Light Mode'}</i>
         <Navbar />
-        <Route exact path = "/" component ={Banner} />
         <Switch>
-          <Route exact path = "/" component ={Banner} />
+          <Route exact path = {["/","/templatedesign"]} component={Banner} />
           <Route path="/profile" component={Profile}/>
           <Route path="/contact" component={Contact}/>
         </Switch>
